dd624a8fba18dd4258ec089eb03a3c77f01d3d55
MMC2K09 <mmc2k09@gmail.com>
2024-12-19 04:13:50 +0600

    update components/logo.py

---
+++ components/logo.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+def add_logo(canvas, logo_path, padding=(20, 20)):
+    try:
+        logo = Image.open(logo_path).convert("RGBA")
+        logo_width, logo_height = logo.size
+        position = (canvas.width - logo_width - padding[0], padding[1])
+        canvas.paste(logo, position, logo)
+    except IOError:
+        return canvas  # If logo is missing, just return the canvas
