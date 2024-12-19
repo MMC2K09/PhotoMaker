377c9f7988c609992b3caa92ab3368c1a07799c7
MMC2K09 <mmc2k09@gmail.com>
2024-12-19 12:47:02 +0600

    update components/canvas.py

--- components/canvas.py
+++ components/canvas.py
@@ -1,5 +1,6 @@
 from PIL import Image
 
 def create_canvas(size, color):
+    """Creates a blank canvas with the given size and background color."""
     color_rgb = tuple(int(color[i:i + 2], 16) for i in (1, 3, 5))
     return Image.new("RGBA", size, color_rgb)
